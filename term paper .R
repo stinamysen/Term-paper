576e6b1a294c64213804a7cbd8e13f29f9ebf41b
stinamysen <stinamysen@live.no>
2020-08-31 17:15:16 +0200

    The term paper files

---
+++ term paper .R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(magrittr)
+library(tidyverse)
+library(readr)
+
+nba <-read.csv("basket.csv") %>% 
+  filter(League=="NBA") %>% 
+  select(Season, Player, Stage, MIN, FGM, FGA, X3PM, X3PA, FTM, FTA) %>% 
+  filter(Stage !="Playoffs") %>% 
+  mutate(h_rate= FGM/FGA)
+
+
+
+best<- function(data, players) {
+  h.rate<- data %>% 
+    filter(Player%in%players) %>% 
+    ggplot(aes(x=Season, y=h_rate, group=Player, colour=Player))+
+    geom_line()
+  
+  return(h.rate)
+}
+
+best(data, players)
