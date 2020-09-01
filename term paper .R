46ec492e4febf419b07d4222061cf413d2db8507
Elisabeth Thornton <elisabethmthornton@gmail.com>
2020-09-01 17:30:25 +0200

    a

--- term paper .R
+++ term paper .R
@@ -1,3 +1,4 @@
+# libraries:
 library(gganimate)
 library(dplyr)
 library(readr)
@@ -5,6 +6,9 @@ library(ggplot2)
 library(magrittr)
 library(tidyverse)
 library(readr)
+library(car)
+library(gifski)
+
 
 nba <-read.csv("basket.csv") %>% 
   filter(League=="NBA") %>% 
@@ -14,15 +18,36 @@ nba <-read.csv("basket.csv") %>%
   mutate(poeng_pr_k = FGM/GP) %>%
   mutate(poeng_pr_min = FGM/MIN) %>%
   mutate(block_pr_k = BLK/GP) %>%
-  mutate(block_pr_min = BLK/MIN)
+  mutate(block_pr_min = BLK/MIN) %>%
+  mutate(Season = recode(
+    Season,
+    "'2009 - 2010' = '2009';
+    '2010 - 2011' = '2010';
+    '2011 - 2012' = '2011';
+    '2012 - 2013' = '2012';
+    '2013 - 2014' = '2013';
+    '2014 - 2015' = '2014';
+    '2015 - 2016' = '2015';
+    '2016 - 2017' = '2016';
+    '2017 - 2018' = '2017';
+    '2018 - 2019' = '2018'"
+  )) %>%
+  mutate(Season = as.numeric(Season))
 
 best<- function(data, players) {
   h.rate<- data %>% 
     filter(Player%in%players) %>% 
     ggplot(aes(x=Season, y=h_rate, group=Player, colour=Player))+
-    geom_line()
+    geom_line() +
+    transition_reveal(Season)
   
   return(h.rate)
 }
 
-best(data, players)
+g <- best(data=nba, players=c("LeBron James", "Kevin Durant"))
+
+#Animate g
+animate(g, duration = 5, fps = 20, width = 650, height = 450, renderer = gifski_renderer())
+
+# Save at gif:
+anim_save("Basketball_timeilne.gif")
